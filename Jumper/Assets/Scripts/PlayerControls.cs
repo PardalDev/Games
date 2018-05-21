53083ef608cea74e3a30a92f879df5fd172cd80b
Nimrod <PardalDevelopers@gmail.com>
2018-05-21 01:22:36 -0300

    Commit Para movimiento
    Se comitea movimiento...ahora se juega con teclas a y d

--- Jumper/Assets/Scripts/PlayerControls.cs
+++ Jumper/Assets/Scripts/PlayerControls.cs
@@ -19,17 +19,40 @@ public class PlayerControls : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        rb.velocity = new Vector2(4, rb.velocity.y);
+        //Inicio sin velocidad en ninguno de los dos ejes
+        rb.velocity = new Vector2(0, rb.velocity.y);
         onGround = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
 
-        if (Input.GetMouseButtonDown(0) & onGround)
+        if (Input.GetKey(KeyCode.D)) {
+            if (GetComponent<SpriteRenderer>().flipX == true)
+            {
+                GetComponent<SpriteRenderer>().flipX = false;
+            }
+            GetComponent<Animator>().SetBool("Corriendo", true);
+            transform.Translate(0.09f, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            if (GetComponent<SpriteRenderer>().flipX == false)
+            {
+                GetComponent<SpriteRenderer>().flipX = true;
+            }
+            GetComponent<Animator>().SetBool("Corriendo", true);
+            transform.Translate(-0.09f, 0, 0);
+        }
+        if (Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.D)){
+            GetComponent<Animator>().SetBool("Corriendo", false);
+        }
+
+
+            //Si clickeo y estoy en tierra salto!
+            if (Input.GetMouseButtonDown(0) & onGround)
         {
             rb.velocity = new Vector2(rb.velocity.x, 5);
         }
-
+        //ademas si clickeo disparo
         bool shoot = Input.GetButtonDown("Fire1");
         shoot |= Input.GetButtonDown("Fire2");
-
         if (shoot) {
             WeaponScript weapon = GetComponent<WeaponScript>();
             if (weapon != null) {
@@ -38,32 +61,28 @@ public class PlayerControls : MonoBehaviour
             }
         }
 
-
-        // 6 - Make sure we are not outside the camera bounds
+        //---------------------------------------------------------------------------------
+        // 6 - Esto no esta en uso, pero es para controlar que siempre estes dentro de la camara 
+        //(al dope en este caso porque la camara sigue al personaje....pero podria no hacerlo)
         var dist = (transform.position - Camera.main.transform.position).z;
-
         var leftBorder = Camera.main.ViewportToWorldPoint(
           new Vector3(0, 0, dist)
         ).x;
-
         var rightBorder = Camera.main.ViewportToWorldPoint(
           new Vector3(1, 0, dist)
         ).x;
-
         var topBorder = Camera.main.ViewportToWorldPoint(
           new Vector3(0, 0, dist)
         ).y;
-
         var bottomBorder = Camera.main.ViewportToWorldPoint(
           new Vector3(0, 1, dist)
         ).y;
-
         transform.position = new Vector3(
           Mathf.Clamp(transform.position.x, leftBorder, rightBorder),
           Mathf.Clamp(transform.position.y, topBorder, bottomBorder),
           transform.position.z
         );
-
+        //---------------------------------------------------------------------------------
     }
     void OnCollisionEnter2D(Collision2D collision)
     {
