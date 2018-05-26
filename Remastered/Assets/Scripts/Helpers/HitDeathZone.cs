09229ed9569743d1d964b899c06dd40492fd75cb
Nimrod <PardalDevelopers@gmail.com>
2018-05-26 04:51:27 -0300

    Se agrega Deadzone
    Se agrega zona de muerte

---
+++ Remastered/Assets/Scripts/Helpers/HitDeathZone.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitDeathZone : MonoBehaviour {
+
+     void OnTriggerEnter2D(Collider2D otherCollider)
+    {
+        GameObject player = GameObject.Find("Player");
+        Destroy(player);
+
+      }
+}
