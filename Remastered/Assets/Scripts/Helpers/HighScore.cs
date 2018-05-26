6b1ff377aef4a8fd381a47122f27dd1d8e4e5d2a
Nimrod <PardalDevelopers@gmail.com>
2018-05-26 01:55:10 -0300

    RemasteredVersion
    Se commitea version 2 de juego completo

---
+++ Remastered/Assets/Scripts/Helpers/HighScore.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class HighScore : MonoBehaviour {
+
+    Text highscore;
+	void Start () {
+        highscore = GetComponent<Text>();
+        highscore.text = "Puntaje Maximo:" + PlayerPrefs.GetInt("HighScore", 0).ToString();
+    }
+	
+	void Update () {
+        if (Score.scorevalue > PlayerPrefs.GetInt("HighScore", 0))
+        {
+            PlayerPrefs.SetInt("HighScore", Score.scorevalue);
+            highscore.text = "Puntaje Maximo:" + PlayerPrefs.GetInt("HighScore", 0).ToString();
+        }	
+	}
+
+}
---
+++ Remastered/Assets/Scripts/MenuScripts/MainMenuScript.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// Title screen script
+/// </summary>
+public class MainMenuScript : MonoBehaviour
+{
+    public void StartGame()
+    {
+        // "Stage1" is the name of the first scene we created.
+        SceneManager.LoadScene("Level1");
+    }
+}
