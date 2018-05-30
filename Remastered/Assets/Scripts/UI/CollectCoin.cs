3944608cf0bee6f5fcbaf2f48183c4a6e923903c
carlmach <39677046+carlmach@users.noreply.github.com>
2018-05-29 22:01:19 -0300

    Added coin picking, and coin count

---
+++ Remastered/Assets/Scripts/UI/CollectCoin.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectCoin : MonoBehaviour {
+
+    public int cantidad = 1;
+
+    public void OnTriggerEnter(Collider Player)
+    {
+
+        Destroy(gameObject);
+
+        if (Player.CompareTag("Player"))
+        {
+            Player.GetComponent<Controlador>().RecogerObjeto(cantidad);
+
+            
+        }
+
+        
+    }
+
+    
+}
---
+++ Remastered/Assets/Scripts/UI/Controlador.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controlador : MonoBehaviour {
+
+    public int CantidadRecogida = 0;
+
+    public void RecogerObjeto(int cantidad)
+    {
+        CantidadRecogida += cantidad;
+    }
+
+}
+
