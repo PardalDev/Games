a64fa3d349dfac32be9d2daa4f7962766f1123b5
Pardal Developers <PardalDevelopers@gmail.com>
2019-05-31 23:23:24 -0300

    Se sube FirstGameDesign
    Va nuevo proyecto

---
+++ FirstGameDesign/Assets/Scripts/Player.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody2D rb;
+    private Vector2 moveAmount;
+
+    private Animator anim;
+
+    public int health;
+    public Image[] hearts;
+    public Sprite fullHeart;
+    public Sprite emptyHeart;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        Vector2 moveImput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        moveAmount = moveImput.normalized * speed;
+
+        //Chequeo si se esta moviendo
+        if (moveImput != Vector2.zero)
+        {
+
+            if (Input.GetKeyDown("left shift")) {
+                anim.SetBool("isWalking", true);
+                anim.SetBool("isAlsoRunning", true);
+            }
+            else {
+                anim.SetBool("isWalking", true);
+                anim.SetBool("isAlsoRunning", false);
+            }
+            
+        }
+        else {
+            anim.SetBool("isWalking", false);
+            anim.SetBool("isAlsoRunning", false);
+        }
+    }
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + moveAmount * Time.fixedDeltaTime);
+    }
+    public void takeDamage(int damageAmount)
+    {
+        health -= damageAmount;
+        UpdateHealthUI(health);
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+    public void ChangeWeapon(WeaponScript weapontoEquip) {
+        Transform weaponPlace = GameObject.FindGameObjectWithTag("Weapon").transform;
+        Destroy(GameObject.FindGameObjectWithTag("Weapon"));
+
+        weapontoEquip.tag = "Weapon";
+        
+        weapontoEquip.transform.localScale += new Vector3(0.7F, 0.7F, 0);
+        
+        Instantiate(weapontoEquip, weaponPlace.position, weaponPlace.rotation, transform);
+        
+    }
+    void UpdateHealthUI(int currentHealth) {
+        for (int i = 0; i < hearts.Length; i++) {
+            if (i < currentHealth)
+            {
+                hearts[i].sprite = fullHeart;
+            }
+            else {
+                hearts[i].sprite = emptyHeart;
+            }
+        }
+    }
+
+    public void Heal(int healAmount) {
+        if (health + healAmount > 5)
+        {
+            health = 5;
+        }
+        else {
+            health += healAmount;
+        }
+        UpdateHealthUI(health);
+    }
+
+    /*
+    GameObject GetChildWithName(GameObject obj, string name)
+    {
+        Transform trans = obj.transform;
+        Transform childTrans = trans.Find(name);
+        if (childTrans != null)
+        {
+            return childTrans.gameObject;
+        }
+        else
+        {
+            return null;
+        }
+    }*/
+
+}
---
+++ FirstGameDesign/Assets/Scripts/WeaponScript.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponScript : MonoBehaviour
+{
+    public GameObject proyectile;
+    public Transform shotPoint;
+    public float timeBetweenShots;
+    private Enemy enemyScript;
+    public int damage;
+
+    public GameObject slash;
+
+    private float shotTime;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(gameObject.transform.root.gameObject.name == "Player") { 
+            Vector2 direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            Quaternion rotation = Quaternion.AngleAxis(angle -90,Vector3.forward);
+            transform.rotation = rotation;
+
+            //0 left mouse button
+            if (Input.GetMouseButton(0)) {
+                if (Time.time >= shotTime) {
+                    Instantiate(proyectile, shotPoint.position, transform.rotation);
+                    shotTime = Time.time + timeBetweenShots;
+                }
+            }
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            enemyScript = collision.gameObject.GetComponent<Enemy>();
+            Instantiate(slash, collision.gameObject.GetComponent<Transform>().position, collision.gameObject.GetComponent<Transform>().rotation);
+            enemyScript.takeDamage(damage);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            enemyScript = collision.gameObject.GetComponent<Enemy>();
+            Instantiate(slash, collision.gameObject.GetComponent<Transform>().position, collision.gameObject.GetComponent<Transform>().rotation);
+            enemyScript.takeDamage(damage);
+        }
+    }
+
+}
