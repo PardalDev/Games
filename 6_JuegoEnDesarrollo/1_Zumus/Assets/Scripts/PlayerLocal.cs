2892d18d26dd321c1576a202288c74fa91e43ec9
Pardal Developers <PardalDevelopers@gmail.com>
2020-10-22 05:19:18 -0300

    Personaje2
    Se committea segundo PJ

--- 6_JuegoEnDesarrollo/1_Zumus/Assets/Scripts/PlayerLocal.cs
+++ 6_JuegoEnDesarrollo/1_Zumus/Assets/Scripts/PlayerLocal.cs
@@ -7,12 +7,15 @@ public class PlayerLocal : MonoBehaviour
     protected Joystick joystick;
     protected Rigidbody2D rb;
 
+    private Animator anim;
+
 
     // Start is called before the first frame update
     void Start()
     {
         joystick = FindObjectOfType<Joystick>();
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
 
     }
 
@@ -25,21 +28,26 @@ public class PlayerLocal : MonoBehaviour
         Vector2 moveImput = new Vector2(Input.GetAxisRaw("Horizontal") + joystick.Horizontal, Input.GetAxisRaw("Vertical") + joystick.Vertical);
         moveAmount = moveImput.normalized * speed;
 
+        //Chequeo si se esta moviendo
         if (moveImput != Vector2.zero)
         {
 
             if (Input.GetKeyDown("left shift"))
             {
-                //   
+                //anim.SetBool("isWalking", true);
+                anim.SetBool("is_running", true);
             }
             else
             {
-                //
+                //anim.SetBool("isWalking", true);
+                anim.SetBool("is_running", true);
             }
+
         }
         else
         {
-            //
+            //anim.SetBool("isWalking", false);
+            anim.SetBool("is_running", false);
         }
 
 
@@ -48,5 +56,6 @@ public class PlayerLocal : MonoBehaviour
     private void FixedUpdate()
     {
             rb.MovePosition(rb.position + moveAmount * Time.fixedDeltaTime);
+            
     }
 }
